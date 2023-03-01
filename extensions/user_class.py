a409f9f0a5d0119e7dadb40d67027a657cafb552
seedsensation <84081520+seedsensation@users.noreply.github.com>
2023-03-01 10:09:51 +0000

    Cleaned up user_class.py
    Added comments, cleaned up convertlegacyfile()

--- extensions/user_class.py
+++ extensions/user_class.py
@@ -1,31 +1,34 @@
 import os
 import pickle
 import pathlib
+import config
 
-attributes = ["id","coins","style","lasttrick"]
+attributes = ["id","coins","style","lasttrick"] # these are the valid attribute names
 
-class Collector:
-    def __init__(self,id):
-        self.id = id
+class Collector: # user class
+    def __init__(self,id): # ID must be provided when instantiating class
+        self.id = id # save ID
         self.coins = 0
 
-    def __getattr__(self,attr):
-        if attr in attributes:
-            self.__dict__[attr] = 0
+    def __getattr__(self,attr): # this runs instead of an AttributeError if attribute is not found
+        if attr in attributes: # if the attribute given is a valid name
+            self.__dict__[attr] = 0 # set the value to 0, return it
             return 0
-        else:
+        else: # otherwise
             # Default behaviour
-            raise AttributeError
+            raise AttributeError # raise an error
 
-    def __repr__(self):
+    def __repr__(self): # this shows when print(userlist) is run - useful for debugging
         return f"Collector with ID {self.id}"
 
-class SearchableList(list):
-    def find(self, userid):
-        return next((x for x in self if x.id == userid), None)
+class SearchableList(list): # new class can be used as a standard Python list, with additional functionality
+    def find(self, userid): # iterates through userlist to find a user with the given ID
+        return next((x for x in self if x.id == userid), None) 
 
-    def savestate(self):
-        listtosave = self.convtolist()
+    def savestate(self): # saves list to SaveState.bin
+        listtosave = self.convtolist() # converts to a standard list first
+        # It's converted to a standard list as this means that I can add additional functionality
+        # to the SearchableList class without having to recreate this list
         with open("SaveState.bin", "wb") as file:
             pickle.dump(listtosave, file)
         print(f"Saved State {self}")
@@ -35,24 +38,28 @@ class SearchableList(list):
             try:
                 with open("SaveState.bin", "rb") as f:
                     loaded = pickle.load(f)
-                    for item in loaded:
-                        self.append(item)
-                        print(f"Loaded user ID {item.id}")
-            except EOFError:
-                os.remove("SaveState.bin")
+                    for item in loaded: # for each item in the loaded (standard) list
+                        if not self.find(item.id):
+                            self.append(item) # add this item to SearchableList if not already present
+                            print(f"Loaded user ID {item.id}")
+                        else:
+                            print(f"User ID {item.id} already in list")
+            except EOFError: # if there's been an error saving the file
+                # EOFError only shows up if the file is empty
+                os.remove("SaveState.bin") # delete the file
 
-        for guild in client.guilds:
-            for member in guild.members:
-                if self.find(member.id):
+        for guild in client.guilds: # for each server the bot is in
+            for member in guild.members: # for each member in those servers
+                if self.find(member.id): # if this member is already in the list, skip
                     print(f"Skipping {member.id}")
-                else:
+                else: # otherwise, add
                     print(f"Adding {member.id}")
                     self.append(Collector(member.id))
 
-        self.savestate()
+        self.savestate() # save result
 
 
-    def convtolist(self):
+    def convtolist(self): # convert self to a list, return result
         result = list()
         for item in self:
             result.append(item)
@@ -65,12 +72,11 @@ def convertlegacyfile():
     with open(pathlib.Path("files/text files/coins.txt"), "r") as f:
         legacyfile = f.read()
 
-    newformat = [collector(userid), collector(userid2), ...]
-
     for item in legacyfile:
-        user = collector(legacyfile[item]) # creates a new Collector item with the ID of the current user
-        user.coins = legacyfile[item][0]
-        user.style = legacyfile[item][1]
-
-    legacyfile.savestate()
+        user = Collector(legacyfile[item]) # creates a new Collector item with the ID of the current user
+        user.coins = legacyfile[item][0]   # sets the new Collector item's coins to their old coin count
+        user.style = legacyfile[item][1]   # sets the new Collector item's StylePoints™ to their old count
+        config.userlist.append(user)
+    
+    config.userlist.savestate() # save result
     
